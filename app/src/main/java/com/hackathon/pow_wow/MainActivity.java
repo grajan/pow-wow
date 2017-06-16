007549cf16b12647a607cfbce19a033e36d091ef
gokulrajan12 <gokulrajan12@gmail.com>
2017-06-16 18:00:04 +0200

    added files

---
+++ app/src/main/java/com/hackathon/pow_wow/MainActivity.java
@@ -0,0 +1,26 @@
+package com.hackathon.pow_wow;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Method invoked after user logged in
+     * @param view the button which the user tapped
+     */
+    public void onLogin(View view) {
+
+        Intent meetUpIntent = new Intent(this, MeetUpActivity.class):
+        startActivity(meetUpIntent);
+
+    }
+}
